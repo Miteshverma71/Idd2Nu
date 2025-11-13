d588f56252894de7bf08abe310cca54c3ef4c2dd
MITESHVERMA <58387869+Miteshverma71@users.noreply.github.com>
2025-11-13 12:02:35 +0530

    Add files via upload

---
+++ annotation/ego_pose.py
@@ -0,0 +1,18 @@
+import json
+
+ego_pose_path = "/home/miteshv/Downloads/av2_train000_5scenes/argov2_1/city_SE3_egovehicle.json"
+
+def generate_ego_pose_json(output_path, ego_pose_data, tokens):
+    poses = []
+    for i, pose in enumerate(ego_pose_data):
+        poses.append({
+            "token": tokens.get(f"ego_{i}"),
+            "timestamp": pose["timestamp_ns"],
+            "translation": [pose["tx_m"], pose["ty_m"], pose["tz_m"]],
+            "rotation": [pose["qx"], pose["qy"], pose["qz"], pose["qw"]]
+        })
+    
+    with open(output_path, "w") as f:
+        json.dump(poses, f, indent=4)
+    
+    print(f"✅ {output_path} created successfully!")
