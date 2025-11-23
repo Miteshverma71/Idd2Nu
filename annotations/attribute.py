cc8cbbfad366a97af0a608c04ffbecd40249c417
MITESHVERMA <58387869+Miteshverma71@users.noreply.github.com>
2025-11-23 22:43:29 +0530

    Update attribute.py

--- annotations/attribute.py
+++ annotations/attribute.py
@@ -1,10 +1,13 @@
 import json
 
-def generate_attribute_json(path, tokens):
+def generate_attribute_json(output_path, tokens, return_data=False):
     attrs = [
         {"token": tokens.get("attr_moving"), "name": "vehicle.moving", "description": "Moving vehicle"},
         {"token": tokens.get("attr_stopped"), "name": "vehicle.stopped", "description": "Stopped vehicle"}
     ]
-    with open(path, "w") as f:
+    if return_data:
+        return attrs
+        
+    with open(output_path, 'w') as f:
         json.dump(attrs, f, indent=4)
     print("✅ attribute.json created")
